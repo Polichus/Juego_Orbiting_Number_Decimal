272939e975939694b40d523e2f9b3b8bd177439f
Pol González Martínez <polgonzalez@ceroca.cat>
2024-10-17 17:32:09 +0200

    escenas bases iniciadas

---
+++ Assets/Scrips/Niveles.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Niveles : MonoBehaviour
+{
+    public void AnarAEscenaInicio()
+    {
+        SceneManager.LoadScene("PantallaInicial");
+    }
+
+    public void AnarAEscenaNivel1()
+    {
+        SceneManager.LoadScene("Nivel1");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
